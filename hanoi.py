047afbfae86184679ee134f21855db3e4fd07b85
xxllea <xxllea@qq.com>
2021-04-01 17:22:17 +0800

    汉诺塔

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+
+index = 1
+
+def hanoi(n, A, B, C):
+    if n > 0:
+        hanoi(n - 1, A, C, B)
+        global index
+        print("第%d步：%s moving to %s" % (index, A, C))
+        index += 1
+        hanoi(n - 1, B, A, C)
+
+hanoi(3, 'A', 'B', 'C')
+
